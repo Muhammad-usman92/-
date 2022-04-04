0abd712ad59fd65c8394b330d2ad13a23072b60f
Muhammad-Usman92 <kusman2560@gmail.com>
2022-04-04 15:30:29 -0700

    Product modal chaange

--- models/Products.model.js
+++ models/Products.model.js
@@ -1,28 +1,83 @@
+
 const Sequelize = require("sequelize");
 const { connection } = require("../config/db");
 // const productController = require("../controllers/product.controller");
 const Category=require("./CategoryModal")
 
 const Product = connection.define("product", {
-	title: {
+	title:{
+		type:Sequelize.STRING
+	},
+	Department: {
 		type: Sequelize.STRING,
 	},
-	imgUrl: {
+	Brand_Name :{
 		type: Sequelize.STRING,
 	},
-	price: {
-		type: Sequelize.INTEGER,
-	},
-	description: {
+	Main_Category: {
 		type: Sequelize.STRING,
 	},
-	weight:{
-		type:Sequelize.INTEGER,
+	Sub_Cat: {
+		type: Sequelize.STRING,
+	},
+	Bar_code:{
+		type:Sequelize.STRING,
+	},
+
+	price:{
+		type:Sequelize.INTEGER
 	}
+	,
+	createdAt: {
+		type: Sequelize.DATE,
+		defaultValue: Sequelize.literal('NOW()')
+	  },
+	  updatedAt: {
+		type: Sequelize.DATE,
+		defaultValue: Sequelize.literal('NOW()')
+	  },
+	  imgurl:{
+		  type:Sequelize.STRING,
+		  default:null,
+	  }
+	  ,
+	  weight:{
+		type:Sequelize.INTEGER,
+		default:null
+		  },	
+		 
 });
-// Category.hasMany(Product)
-// Product.hasOne(Category)
+
 
 Product.belongsTo(Category)
 Category.hasMany(Product)
 module.exports = Product;
+
+// const Sequelize = require("sequelize");
+// const { connection } = require("../config/db");
+// // const productController = require("../controllers/product.controller");
+// const Category=require("./CategoryModal")
+
+// const Product = connection.define("product", {
+// 	title: {
+// 		type: Sequelize.STRING,
+// 	},
+// 	imgUrl: {
+// 		type: Sequelize.STRING,
+// 	},
+// 	price: {
+// 		type: Sequelize.INTEGER,
+// 	},
+// 	description: {
+// 		type: Sequelize.STRING,
+// 	},
+// 	weight:{
+// 		type:Sequelize.INTEGER,
+// 	}
+// });
+// // Category.hasMany(Product)
+// // Product.hasOne(Category)
+
+// Product.belongsTo(Category)
+// Category.hasMany(Product)
+// module.exports = Product;
